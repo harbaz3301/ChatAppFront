5040ecc901d0c75708a784df3d1cf778d5955874
harbaz3301 <harbazbrar3223@gmail.com>
2022-08-29 15:39:28 +0530

    Add project files.

---
+++ ChatAppFront/Server/hubs/chathub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppFront.Server.hubs
+{
+    public class chathub : Hub
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+        public override async Task OnConnectedAsync()
+        {
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await sendMessagetoChat(string.Empty, $"{username} : joined chat");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await sendMessagetoChat(string.Empty, $"{username} left!");
+        }
+        public async Task sendMessagetoChat(string user, string message)
+        {
+            await Clients.All.SendAsync("GetThatMessage",user,message);
+        }
+    }
+}
